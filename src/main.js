5a867b1c9888581134d34142692a03d60732a5b8
Codedealer123 <mukilan.2.2014@gmail.com>
2026-05-04 20:36:17 -0500

    Changed how sw is registered

--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ const app = mount(App, {
   target: document.getElementById('app'),
 })
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js')
+      console.log('Service Worker successfully registered:', registration.scope)
+    } catch (error) {
+      console.error('Service Worker registration failed:', error)
+    }
+  })
+}
 
 
 export default app
